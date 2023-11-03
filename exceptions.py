6352eb3a48676f2b4fa52477ae50a2e45935cf31
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2023-11-03 13:24:34 -0300

    transform data

--- exceptions.py
+++ exceptions.py
@@ -1,5 +1,31 @@
+"""Exceptions used in the package."""
+
+
 class APIRequestException(Exception):
     """Exception raised when an API request fails."""
 
     def __init__(self, url: str, response: dict):
         super().__init__(f"Request to '{url}' failed. Response: {response}")
+
+
+class EmptyDataFrameException(Exception):
+    """Exception raised when the DataFrame is empty."""
+
+    def __init__(self):
+        super().__init__("DataFrame is empty.")
+
+
+class NonUniquePrimaryKeyException(Exception):
+    """Exception raised when the primary key is not unique."""
+
+    def __init__(self, column_name: str):
+        super().__init__(
+            f"The '{column_name}' has non unique values. Primary key check failed."
+        )
+
+
+class NullValuesFoundException(Exception):
+    """Exception raised when null values are found in the DataFrame."""
+
+    def __init__(self):
+        super().__init__("Null values found in the DataFrame.")
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-"""Lambda function to get recently played songs from Spotify API and store them in a SQLite database."""
+"""Lambda function to get recently played songs from Spotify API."""
 import pandas as pd
 from sqlalchemy import create_engine
 
+from exceptions import (
+    EmptyDataFrameException,
+    NonUniquePrimaryKeyException,
+    NullValuesFoundException,
+)
 from spotify_etl import get_token, get_recently_played, get_artists_genres
 
 TOKEN = get_token()
@@ -9,6 +14,7 @@ TOKEN = get_token()
 
 def extract() -> pd.core.frame.DataFrame:
     """Extract recently played songs from Spotify API"""
+
     songs = []
     albums = []
     artists = []
@@ -36,6 +42,39 @@ def extract() -> pd.core.frame.DataFrame:
     )
 
 
+def check_data_integrity(df: pd.core.frame.DataFrame):
+    """Check if dataframe is empty, if primary key is unique and if there are null values."""
+
+    if df.empty:
+        raise EmptyDataFrameException()
+
+    if not pd.Series(df["played_at"]).is_unique:
+        raise NonUniquePrimaryKeyException(column_name="played_at")
+
+    if df.isnull().values.any():
+        raise NullValuesFoundException()
+
+
+def transform(df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
+    """Transform recently played songs from Spotify API
+
+    - Add artist genres column to the DataFrame
+    """
+
+    check_data_integrity(df=df)
+
+    df["genres"] = df["artist_id"].apply(
+        lambda artist_id: get_artists_genres(artist_id, TOKEN)
+    )
+    df["genres"] = [", ".join(genre) for genre in df["genres"]]
+
+    df["genres"] = df["genres"].replace("", None)
+    df = df.dropna(subset=["genres"])
+
+    return df
+
 
 if __name__ == "__main__":
     recently_played = extract()
+
+    transformed_data = transform(df=recently_played)
