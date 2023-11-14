c7d58844a5c84410941f0c052486a4d70d5a76d4
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2023-11-14 15:49:42 -0300

    good bash script

--- terraform/aws_plan.sh
+++ terraform/aws_plan.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 
-DEPLOYMENT_DIR="../project"
 TERRAFORM_DIR=$(pwd)
+PROJECT_DIR=$(dirname "$TERRAFORM_DIR")
+DEPLOY_DIR="$PROJECT_DIR/deploy"
+PKG_DIR="$PROJECT_DIR/python"
 
-terraform init
+# Removing previous files
 
-pip install -r ../requirements.txt -t $DEPLOYMENT_DIR
+rm $PROJECT_DIR/*.zip
 
-cp ../main.py $DEPLOYMENT_DIR
-cp ../spotify_etl.py $DEPLOYMENT_DIR
-cp ../exceptions.py $DEPLOYMENT_DIR
+# Requests layer
 
-cd $DEPLOYMENT_DIR
+rm -rf $PKG_DIR && mkdir -p $PKG_DIR
 
-zip -r ../lambda.zip *
+pip install requests -t $PKG_DIR
+
+rm -rf $PKG_DIR/*.dist-info $PKG_DIR/__pycache__
+
+cd $PROJECT_DIR
+zip -r9 ./requests.zip ./python
+
+# Lambda function
+
+rm -rf $DEPLOY_DIR && mkdir -p $DEPLOY_DIR
+
+cp $PROJECT_DIR/lambda_function.py $DEPLOY_DIR
+cp $PROJECT_DIR/spotify_etl.py $DEPLOY_DIR
+cp $PROJECT_DIR/exceptions.py $DEPLOY_DIR
+
+cd $DEPLOY_DIR
+zip -r $PROJECT_DIR/lambda.zip *
+
+# Removing files
+
+rm -rf $PKG_DIR $DEPLOY_DIR
+
+# Terraform
 
 cd $TERRAFORM_DIR
-rm -rf $DEPLOYMENT_DIR
-
+terraform init
 terraform plan
